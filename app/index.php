443dcafe9a6c429986795b645bda287d32854321
AssaneDev <adiop6225@gmail.com>
2023-09-09 18:16:59 +0200

    connexionRequire bd

--- app/index.php
+++ app/index.php
@@ -1,5 +1,5 @@
 <?php 
-require __DIR__.'/database/conexion.php';
+require_once __DIR__.'/database/conexion.php';
 $programmeDB = require_once __DIR__.('/database/models/programmeDB.php');
 
 $listProgramme = $programmeDB->fetchAll();
