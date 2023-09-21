60ec9a4ca53e671cf7dde1fe3ddf182cdccca9fa
AssaneDev <adiop6225@gmail.com>
2023-09-21 17:38:02 +0200

    fixe array

--- app/form.php
+++ app/form.php
@@ -20,7 +20,7 @@ $programmeDB = require_once __DIR__.('/database/models/programmeDB.php');
  ];
 
    $_GET = filter_input_array(INPUT_GET,FILTER_SANITIZE_SPECIAL_CHARS);
-    $idprogramme = $_GET['id'];
+    $idprogramme = $_GET['id'] ?? '';
 
 	if($idprogramme){
 		$programme = $programmeDB->ModifOne($idprogramme);
