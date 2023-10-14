52ee66e300b356abbbc5c21a6f53559ab77be082
AssaneDev <adiop6225@gmail.com>
2023-10-14 21:48:23 +0200

    contact

--- app/include/header.php
+++ app/include/header.php
@@ -17,7 +17,7 @@
             <div class="navigation-brand-text">
               <div class="rlr-logo rlr-logo__navbar-brand rlr-logo--default">
                 <a href="./index.html">
-                  <img src="./assets/svg/amitie.png" style="width:400px; height:200px" alt="#" class="" />
+                  <img src="./assets/svg/amitie.png" style="width:200px; height:75px" alt="#" class="" />
                 </a>
               </div>
             </div>
